16303387400df9f3cbfbbda2834ab5c53cd2db54
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-05-30 19:06:58 -0300

    asosiacion de arrays

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,38 @@
 <?php include 'includes/header.php';
 
+//accedes a ellos por medio de una propiedad
+$cliente = [
+    'nombre'=> 'juan',
+    'saldo'=> 200,
+    'informacion'=>[
+        'tipo'=> 'premiun',
+        'disponible'=> '100',
+    ],
+];
 
+// accediendo a la propiedad nombre 
+echo "<pre>";
+var_dump($clientes['nombre']);
+"</pre>";
 
+echo '<br>';
+//accediendo al array dentro del array
+echo "<pre>";
+var_dump($clientes['informacion']['tipo']);
+"</pre>";
+echo '<br>';
+// o 
+
+echo $clientes['informacion']['tipo'];
+echo '<br>';
+echo $clientes['informacion']['disponible'];
+
+//agregar mas propiedades al arreglo
+
+$cliente['codigo'] = 123456789;
+
+echo "<pre>";
+var_dump($cliente);
+"</pre>";
 
 include 'includes/footer.php';
