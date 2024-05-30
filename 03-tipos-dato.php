e01566155cb535df4fe1dc1b96ff19c34a7af9b2
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-05-30 09:49:21 -0300

    tipos de datos

--- 03-tipos-dato.php
+++ 03-tipos-dato.php
@@ -1,6 +1,30 @@
 <?php include 'includes/header.php';
 
+//boolean
 
+$logueado = true;
+
+var_dump($logueado);
+
+// ENTEROS 
+
+$numero = 200;
+var_dump($numero);
+
+
+// FLOTANTES
+$float = 200.5;
+var_dump($float);
+
+
+// string
+
+$nombre = 'juan';
+var_dump($nombre);
+
+// arreglos
+$array = [];
+var_dump($array);
 
 
 include 'includes/footer.php';
