f88610c70391cf37677d8588c76dfa522fa98c29
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-05-30 11:52:56 -0300

    operadores

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,5 +1,28 @@
 <?php include 'includes/header.php';
 
+$numero1 = 20;
+$numero2 = 10;
+
+echo $numero1 + $numero2;
+
+echo '<br>';
+
+echo $numero1 - $numero2;
+
+echo '<br>';
+
+echo $numero1 * 2 ;
+
+// dividir 
+echo '<br>';
+
+echo $numero1 / $numero2;
+
+echo '<br>';
+
+
+// 3 veces el valor de 2 
+echo 2 ** 3;
 
 
 
