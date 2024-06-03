dff63ead1087728cffc6dcf311afe08a950497e6
pablitomartinez <86635786+pablitomartinez@users.noreply.github.com>
2024-06-03 15:55:38 -0300

    utilizar ternarios

--- 14-foreach.php
+++ 14-foreach.php
@@ -1,5 +1,32 @@
 <?php include 'includes/header.php';
+$productos = [
+    ['nombre' => 'Tablet',
+    'precio' => 200,
+    'disponible' => true,
+],
+    ['nombre' => 'television',
+    'precio' => 300,
+    'disponible' => true,
+],
+    ['nombre' => 'phone',
+    'precio' => 1500,
+    'disponible' => false,
+],
 
+];
+
+//PHP EN HTML
+foreach($productos as $producto){ ?>
+    <li>
+        <p>Producto: <?php echo $producto['nombre'];  ?> </p>
+        <p>Precio: <?php echo '$ '. $producto['precio'];  ?> </p>
+        <p>Disponible: <?php echo ($producto['disponible'] ? 'Disponible' : 'No disponible');  ?> </p>
+    </li>
+    <?php
+    // echo '<pre>';
+    // var_dump($producto);
+    // echo '</pre>';
+}
 
 
 
